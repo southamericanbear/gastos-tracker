fc616c95b2ae59c441ee9bb44384ca16f39470f3
alan Dittler <dittler.a@gmail.com>
2020-12-09 20:02:48 -0300

    front end finished

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
 
 export default function Transaction({ transaction }) {
   const sign = transaction.amount < 0 ? "-" : "+";
+
+  const { deleteTransaction } = useContext(GlobalContext);
 
   return (
     <li className={transaction.amount < 0 ? "minus" : "plus"}>
@@ -10,7 +12,12 @@ export default function Transaction({ transaction }) {
       <span>
         {sign}${Math.abs(transaction.amount)}{" "}
       </span>
-      <button className="delete-btn">x</button>
+      <button
+        onClick={() => deleteTransaction(transaction.id)}
+        className="delete-btn"
+      >
+        x
+      </button>
     </li>
   );
 }
